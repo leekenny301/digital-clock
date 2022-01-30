f94782912b6eb73455dca4fd58543ce81fd6bde3
leekenny301 <leekenny301@gmail.com>
2022-01-30 15:44:27 +0800

    added a clock function that displays system time in 24hour format

--- digital_clock.py
+++ digital_clock.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
@@ -9,4 +10,12 @@ label = Label(window, font=(
     "Arial Black", 78, "bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500, clock)
+
+
+clock()
 window.mainloop()
