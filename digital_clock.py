b34719ec588e72fb3ae642255fd32a50537dcc55
leekenny301 <leekenny301@gmail.com>
2022-01-30 15:29:27 +0800

    added welcome message and blue bg to digital clock

--- digital_clock.py
+++ digital_clock.py
@@ -3,5 +3,10 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+
+label = Label(window, text='Welcome!', font=(
+    "Arial Black", 78, "bold"), bg="steelblue", fg="white")
+label.pack(pady=100)
 
 window.mainloop()
