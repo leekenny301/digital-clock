83b043f90d368b651d897b6b232dd911801b4ff9
leekenny301 <leekenny301@gmail.com>
2022-01-30 15:30:26 +0800

    removed welcome message from digital clock

--- digital_clock.py
+++ digital_clock.py
@@ -5,7 +5,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, text='Welcome!', font=(
+label = Label(window, font=(
     "Arial Black", 78, "bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 
